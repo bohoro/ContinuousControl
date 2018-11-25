b2ba406eb05d6312e5449817cde5be9fc9bc851b
Brian O'Horo <brian.ohoro@gmail.com>
2018-11-25 10:54:42 -0800

    Removed commented code

--- continuous_control.py
+++ continuous_control.py
@@ -37,30 +37,7 @@ print(
 )
 print("The state for the first agent looks like:", states[0])
 
-"""
-# Take Random Actions in the Environment
-env_info = env.reset(train_mode=False)[brain_name]  # reset the environment
-states = env_info.vector_observations  # get the current state (for each agent)
-scores = np.zeros(num_agents)  # initialize the score (for each agent)
-for _ in range(100):
-    actions = np.random.randn(
-        num_agents, action_size
-    )  # select an action (for each agent)
-    actions = np.clip(actions, -1, 1)  # all actions between -1 and 1
-    env_info = env.step(actions)[brain_name]  # send all actions to the env
-    next_states = env_info.vector_observations  # get next state for each agent
-    rewards = env_info.rewards  # get reward (for each agent)
-    dones = env_info.local_done  # see if episode finished
-    scores += env_info.rewards  # update the score (for each agent)
-    states = next_states  # roll over states to next time step
-    print(actions, env_info.rewards)
-    if np.any(dones):  # exit loop if episode finished
-        break
-print("Total score (averaged over agents) this episode: {}".format(np.mean(scores)))
-"""
-
 agent = Agent(state_size=cstate_size, action_size=4, random_seed=42)
-
 
 def ddpg(n_episodes=500, max_t=1000, print_every=10):
     scores_deque = deque(maxlen=print_every)
